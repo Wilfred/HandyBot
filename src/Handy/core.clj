94b491c99bf28609eebbf5885f4c69edc4598e3b
Wilfred Hughes <wilfred@Marmite.(none)>
2012-06-17 19:56:01 +0100

    adding code I'm using to play with the bot

--- src/Handy/core.clj
+++ src/Handy/core.clj
@@ -74,3 +74,9 @@
     (while true
       (println (.readLine (:in server-connection))))
     (.close (:in server-connection))))
+
+;; code for playing with the bot:
+;; (def s (connect-to-server IRC-SERVER PORT))
+;; (idle-in-channel s CHANNEL)
+;; (say-in-channel s CHANNEL "foo")
+;; (disconnect-from-server s)
