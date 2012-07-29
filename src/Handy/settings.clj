d650da462a8299132cac1fac268d5ab5f1a74b49
Wilfred Hughes <me@wilfred.me.uk>
2012-07-29 18:49:01 +0100

    removing unused constants

--- src/Handy/settings.clj
+++ src/Handy/settings.clj
@@ -1,17 +1,6 @@
 (ns Handy.settings)
 
 ;; todo: automatically generate names when there's a name clash
-(def NICK "HandyBot")
-(def USER-NAME "HandyBot")
-(def HOST-NAME "HandyBot")
-(def SERVER-NAME "HandyBot")
-(def REAL-NAME "Handy IRC Bot")
-
-(def IRC-SERVER "irc.freenode.net")
-(def PORT 6667)
-
-(def CHANNEL "#HandyBot")
-
 (def settings
   {:nick "HandyBot"
    :user-name "HandyBot"
