40f72d6a9fa50d926881111a028c164108c61cb1
Wilfred Hughes <me@wilfred.me.uk>
2012-07-29 16:50:17 +0100

    separating parsing functions to a separate file

---
+++ src/Handy/parsing.clj
@@ -0,0 +1,23 @@
+(ns Handy.parsing
+  (:use [clojure.string :only [trim]]))
+
+;; todo: factor out the bot command prefix
+
+(defn parse-irc-message [raw-message]
+  "Separate RAW-MESSAGE into a map of the different parts: the
+message, the user, etc."
+  (let [[whole-message nick user host message-type channel message]
+        (re-find #":(.*?)!(.*?)@(.*?) (.*?) (.*?) :(.*)" raw-message)]
+    {:nick nick :user user :host host :message-type message-type
+     :channel channel :message message}))
+
+(defn parse-bot-message [raw-message]
+  "Separate RAW-MESSAGE into the command name, its argument, and IRC
+information. Returns nil if this raw messsage doesn't look like a bot
+command."
+  (let [parsed-irc-message (parse-irc-message raw-message)]
+    (when-let [match (re-find #"%([^ ]+)(.*)" (parsed-irc-message :message))]
+      (let [[_ command-name argument] match]
+        (conj parsed-irc-message
+              {:command-name command-name :argument (trim argument)})))))
+
--- src/Handy/routing.clj
+++ src/Handy/routing.clj
@@ -1,9 +1,7 @@
 (ns Handy.routing
   "Functions that make the bot speak in a channel."
-  (:use [clojure.string :only [trim]]
-         [Handy.commands.help :only [help]]))
-
-;; todo: factor out the bot command prefix
+  (:use [Handy.parsing :only [parse-bot-message]]
+        [Handy.commands.help :only [help]]))
 
 ;; todo: fix the duplication with connection.clj of send-to-server and join-channel
 (defn send-to-server [server-connection raw-message]
@@ -13,24 +11,6 @@
 
 (defn say-in-channel [server-connection channel message]
   (send-to-server server-connection (format "PRIVMSG %s :%s" channel message)))
-
-(defn parse-irc-message [raw-message]
-  "Separate RAW-MESSAGE into a map of the different parts: the
-message, the user, etc."
-  (let [[whole-message nick user host message-type channel message]
-        (re-find #":(.*?)!(.*?)@(.*?) (.*?) (.*?) :(.*)" raw-message)]
-    {:nick nick :user user :host host :message-type message-type
-     :channel channel :message message}))
-
-(defn parse-bot-message [raw-message]
-  "Separate RAW-MESSAGE into the command name, its argument, and IRC
-information. Returns nil if this raw messsage doesn't look like a bot
-command."
-  (let [parsed-irc-message (parse-irc-message raw-message)]
-    (when-let [match (re-find #"%([^ ]+)(.*)" (parsed-irc-message :message))]
-      (let [[_ command-name argument] match]
-        (conj parsed-irc-message
-              {:command-name command-name :argument (trim argument)})))))
 
 (defn call-raw-command [server-connection command parsed-message]
   "Execute a bot command COMMAND that was called by
@@ -57,9 +37,6 @@ PARSED-MESSAGE. The command may only say something in the channel."
 (defcommand hello [{nick :nick}]
   "Greet the user who spoke."
   (format "Hello %s!" nick))
-
-(defn join-channel [{channel :argument}]
-  (format "JOIN %s" channel))
 
 (defcommand magic8 [{}]
   "Randomly choose a yes-no-maybe response."
