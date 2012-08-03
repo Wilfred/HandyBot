9541487628b83a635e98ca237e02b74d8d9f8eaf
Wilfred Hughes <me@wilfred.me.uk>
2012-08-03 20:37:44 +0100

    changing project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject Handy "1.0.0-SNAPSHOT"
-  :description "Simple IRC bot"
+  :description "Self-documenting IRC bot"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.1.3"]
                  [org.clojure/data.json "0.1.2"]]
