b7f2bc1e99ce22bc0e984b3a5a4c863665a1f2cd
Wilfred Hughes <me@wilfred.me.uk>
2012-09-06 15:37:04 +0100

    fixing function definition order in %music

--- src/Handy/commands/music.clj
+++ src/Handy/commands/music.clj
@@ -3,18 +3,6 @@
   (:require [clj-http.client :as client])
   (:use [clojure.data.json :only (read-json)]))
 
-(defn music
-  "Suggest similar artists to the one given."
-  [{artist-name :argument}]
-  (let [similar-artists (last-fm-similar-artists artist-name)]
-    (if (not-empty similar-artists)
-      (format "Hmm, if you like %s, you might like %s, %s or even %s."
-              artist-name
-              (nth similar-artists 0 "")
-              (nth similar-artists 1 "")
-              (nth similar-artists 2 ""))
-      (format "Sorry, I haven't heard of %s." artist-name))))
-
 ;; todo: actual api key, not just copy-paste from the docs sample code
 (def api-key "b25b959554ed76058ac220b7b2e0a026")
 
@@ -31,8 +19,14 @@
         artists-names (map :name artists)]
     artists-names))
 
-
-
-
-
-
+(defn music
+  "Suggest similar artists to the one given."
+  [{artist-name :argument}]
+  (let [similar-artists (last-fm-similar-artists artist-name)]
+    (if (not-empty similar-artists)
+      (format "Hmm, if you like %s, you might like %s, %s or even %s."
+              artist-name
+              (nth similar-artists 0 "")
+              (nth similar-artists 1 "")
+              (nth similar-artists 2 ""))
+      (format "Sorry, I haven't heard of %s." artist-name))))
