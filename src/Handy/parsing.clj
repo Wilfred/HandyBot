aa126e741a07bb53d80c553fbdae8d69f01ddfa8
Wilfred Hughes <me@wilfred.me.uk>
2013-02-01 15:44:45 +0000

    docstrings come before argument specs in Clojure

--- src/Handy/parsing.clj
+++ src/Handy/parsing.clj
@@ -3,18 +3,20 @@
 
 ;; todo: factor out the bot command prefix
 
-(defn parse-irc-message [raw-message]
+(defn parse-irc-message
   "Separate RAW-MESSAGE into a map of the different parts: the
 message, the user, etc."
+  [raw-message]
   (let [[whole-message nick user host message-type channel message]
         (re-find #":(.*?)!(.*?)@(.*?) (.*?) (.*?) :(.*)" raw-message)]
     {:nick nick :user user :host host :message-type message-type
      :channel channel :message message}))
 
-(defn parse-bot-message [raw-message]
+(defn parse-bot-message
   "Separate RAW-MESSAGE into the command name, its argument, and IRC
 information. Returns nil if this raw messsage doesn't look like a bot
 command."
+  [raw-message]
   (let [parsed-irc-message (parse-irc-message raw-message)]
     (when-let [match (re-find #"(%[^ ]+)(.*)" (parsed-irc-message :message))]
       (let [[_ command-name argument] match]
