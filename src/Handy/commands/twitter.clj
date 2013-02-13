8c93ce810f8f19b2f0752e0fe71cee9bcda3bf70
Wilfred Hughes <me@wilfred.me.uk>
2013-02-13 12:15:16 +0000

    handle twitter failures

--- src/Handy/commands/twitter.clj
+++ src/Handy/commands/twitter.clj
@@ -2,9 +2,9 @@
   (:require [twitter]))
 
 (defn devops [{}]
-  (let [tweets (twitter/user-timeline :screen-name "DEVOPS_BORAT")
+  ;; ideally, we'd catch a twitter exception, but the library doesn't define its own exception
+  (let [tweets (try
+                 (twitter/user-timeline :screen-name "DEVOPS_BORAT")
+                 (catch Exception e ["Twitter said no, sorry."]))
         tweet (first (shuffle tweets))]
     (:text tweet)))
-
-(defn twitter [{}])
-
