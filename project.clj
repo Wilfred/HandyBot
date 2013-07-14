62cdf07e7b50323d851fced5b9229a0fc7983687
Wilfred Hughes <me@wilfred.me.uk>
2013-07-14 11:07:00 +0100

    move to clojure 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject Handy "1.0.0-SNAPSHOT"
   :description "Self-documenting IRC bot"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.1.3"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-time "0.4.4"]
